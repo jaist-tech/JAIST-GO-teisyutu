7daefb37c81c088599fde96b0e93a6155749470d
Miyako Fujita <fujita.miyako3850303@gmail.com>
2026-06-13 00:30:42 +0900

    demo_pageを作成

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,3 +6,12 @@ def index():
     return render_template(
         'index.html'
         )
+    
+@app.route("/demo/board")
+def demo_board():
+    return render_template("demo_board.html")
+
+
+@app.route("/demo/posts/<int:post_id>")
+def demo_post(post_id):
+    return render_template("demo_post.html", post_id=post_id)
