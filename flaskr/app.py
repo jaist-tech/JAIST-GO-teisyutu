e64f7005306cec1fd70e8efcc6554b5b62f50321
Miyako Fujita <fujita.miyako3850303@gmail.com>
2026-06-12 18:31:24 +0900

    簡単にマップを実装

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,8 +1,17 @@
-from flaskr import app
-from flask import render_template
+from flask import Flask, render_template
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
-    return render_template(
-        'index.html'
-        )
+    location = {
+        "lat": 35.6811673,
+        "lng": 139.7670516,
+        "title": "東京駅"
+    }
+
+    return render_template("index.html", location=location, google_map_api_key=app.config["google_map_api_key"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
